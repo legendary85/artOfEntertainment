22051bb119ee8f01c0c6f47aa69651036561feb2
Kourtney McCullough <kourtneyjmccullough@gmail.com>
2020-02-15 15:16:15 -0600

    added navbar and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import Navbar from './components/Layout/navbar/Navbar.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/navbar/Navbar.js'
 // import Landing from './components/landing/Landing.js'
 // import About from './components/about/About.js'
 // import Packages from './components/packages/Packages.js'
@@ -14,22 +14,20 @@ import './App.css';
 function App() {
   return (
 
-    <div>
-      Art of Entertainment
-    </div>
-    // <Router>
-    //   <Navbar />
-    //   <section>
-    //     <Switch>
-    //       <Route exact path='/' component={Landing}></Route>
-    //       <Route exact path='/about' component={About}></Route>
-    //       <Route exact path='/packages' component={Packages}></Route>
-    //       <Route exact path='/contact' component={ContactForm}></Route>
-    //       {/* <Route exact path='/contact' component={DemoContactForm}></Route> */}
-    //     </Switch>
-    //   </section>
-    //   <Footer style={{ color: 'red' }} />
-    // </Router>
+
+    <Router>
+      <Navbar />
+      <section>
+        <Switch>
+          {/* <Route exact path='/' component={Landing}></Route> */}
+          {/* <Route exact path='/about' component={About}></Route> */}
+          {/* <Route exact path='/packages' component={Packages}></Route> */}
+          {/* <Route exact path='/contact' component={ContactForm}></Route> */}
+          {/* <Route exact path='/contact' component={DemoContactForm}></Route> */}
+        </Switch>
+      </section>
+      {/* <Footer style={{ color: 'red' }} /> */}
+    </Router>
   );
 }
 
